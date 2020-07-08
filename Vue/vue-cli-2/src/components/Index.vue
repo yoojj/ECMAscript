07af49f60836507200191f7359d352e0c7742a3d
yjj <yoojj.xyz@gmail.com>
2020-07-08 19:27:09 +0900

    init

---
+++ Vue/vue-cli-2/src/components/Index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+    name : 'index',
+
+    data() {
+        return {
+            msg : 'index'
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+</style>
